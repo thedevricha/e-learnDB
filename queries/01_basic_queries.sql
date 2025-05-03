56953c53664b90184751eb859df07df9a8db4b0a
Richa <richa.parekh@pavansgroup.in>
2025-05-03 17:19:29 +0530

    Formatting files

--- queries/01_basic_queries.sql
+++ queries/01_basic_queries.sql
@@ -15,48 +15,67 @@ SELECT * FROM enrollments;
 SELECT name, email, role FROM users;
 
 /* 2. Find all students (role = 'student') who were born after January 1, 1997. */
-SELECT users.id, users.name, students.date_of_birth FROM students 
-JOIN users ON students.user_id = users.id
-WHERE users.role = 'student' AND students.date_of_birth > '1997-01-01';
+SELECT u.id, u.name, s.date_of_birth 
+FROM students s
+JOIN users u
+ON s.user_id = u.id
+WHERE u.role = 'student' AND s.date_of_birth > '1997-01-01';
 
 /* 3. Show the names of all instructors along with their area of expertise. */
-SELECT users.id, users.name, instructors.expertise_area FROM instructors
-JOIN users ON instructors.user_id = users.id
-WHERE users.role = 'instructor';         
+SELECT u.id, u.name, i.expertise_area 
+FROM instructors i
+JOIN users u 
+ON i.user_id = u.id
+WHERE u.role = 'instructor';         
 
-/* 4. Get the title and price of all courses in the "Computer Science" category. */
-SELECT courses.title, course_categories.name, courses.price FROM courses
-JOIN course_categories ON courses.category_id = course_categories.id
-WHERE course_categories.name = 'Computer Science';
+/* 4. Get the title and price of all courses in the "Web Development" category. */
+SELECT c.title, cc.name, c.price 
+FROM courses c
+JOIN course_categories cc
+ON c.category_id = cc.id
+WHERE cc.name = 'Web Development';
 
-/* 5. List all lessons for the course "Course 2: Professional Mathematics", ordered by lesson order. */
-SELECT courses.title, lessons.content, lessons.lesson_order FROM lessons
-JOIN courses ON lessons.course_id = courses.id
-WHERE courses.title = 'Course 2: Professional Mathematics'
-ORDER BY lessons.lesson_order;
+/* 5. List all lessons for the course "Object-based needs-based leverage Course", ordered by lesson order. */
+SELECT c.title, l.content, l.lesson_order 
+FROM lessons l
+JOIN courses c
+ON l.course_id = c.id
+WHERE c.title = 'Object-based needs-based leverage Course'
+ORDER BY l.lesson_order;
 
 /* 6. How many lessons are there in each course? */
-SELECT course_id, COUNT(id) as lesson_count FROM lessons GROUP BY course_id;
+SELECT c.title AS course_name, l.course_id, COUNT(l.id) as lesson_count 
+FROM lessons l
+JOIN courses c
+ON l.course_id = c.id
+GROUP BY l.course_id, c.title
+ORDER BY lesson_count DESC;
 
-/* 7. Find all students enrolled in the course "Course 2: Advanced Data Analysis". */
-SELECT users.name FROM enrollments
-JOIN students ON enrollments.student_id = students.id
-JOIN users ON students.user_id = users.id
-JOIN courses ON enrollments.course_id = courses.id
-WHERE courses.title = 'Course 2: Advanced Data Analysis';
+/* 7. Find all students enrolled in the course that include "Multi-channeled" in course title. */
+SELECT u.name AS student_name, c.title AS course_name 
+FROM enrollments e
+JOIN students s
+ON e.student_id = s.id
+JOIN users u
+ON s.user_id = u.id
+JOIN courses c
+ON e.course_id = c.id
+WHERE c.title LIKE '%Multi-channeled%';
 
-/* 8. List the assignments and their due dates for the course "Course 2: Advanced Data Analysis". */
-SELECT courses.title AS course, assignments.title AS assignment, assignments.due_date 
-FROM assignments
-LEFT JOIN courses ON assignments.course_id = courses.id
-WHERE courses.title = 'Course 2: Advanced Data Analysis';
+/* 8. List the assignments and their due dates for the course title start with "Optimized". */
+SELECT c.title AS course, a.title AS assignment, a.due_date 
+FROM assignments a
+LEFT JOIN courses c 
+ON a.course_id = c.id 
+WHERE c.title LIKE 'Optimized%';
 
 /* 9. Show all course reviews with the student ID, course ID, rating, and comment. */
-SELECT reviews.course_id, courses.title AS course, reviews.student_id, reviews.rating, reviews.comment 
-FROM reviews
-LEFT JOIN courses ON reviews.course_id = courses.id;
+SELECT r.course_id, c.title AS course, r.student_id, r.rating, r.comment 
+FROM reviews r
+LEFT JOIN courses c 
+ON r.course_id = c.id;
 
 /* 10. List total payment amount made by each student. (Hint: GROUP BY student_id) */
 SELECT payments.student_id, SUM(payments.amount) AS total_payment 
-FROM payments 
+FROM payments
 GROUP BY student_id;
--- queries/02_intermediate_queries.sql
+++ queries/02_intermediate_queries.sql
@@ -1,47 +1,60 @@
 /* 1. List each course title and the number of students enrolled in it. Include courses even if they have no enrollments */
 SELECT 
-    courses.id, 
-    courses.title AS course, 
-    COUNT(enrollments.student_id) AS count_students 
-FROM courses
-LEFT JOIN enrollments ON courses.id = enrollments.course_id 
-GROUP BY courses.id, courses.title;
+    c.id, 
+    c.title AS course, 
+    COUNT(e.student_id) AS count_students 
+FROM courses c
+LEFT JOIN enrollments e
+ON c.id = e.course_id 
+GROUP BY c.id, c.title
+ORDER BY count_students;
 
 /* 2. Show each instructor’s name and the total number of courses they teach. */
 SELECT u.name AS instructor_name, COUNT(c.id) AS count_courses
-FROM instructors AS i
-JOIN users AS u ON i.user_id = u.id
-JOIN courses AS c ON i.id = c.instructor_id
-GROUP BY u.id, u.name;
+FROM instructors i
+JOIN users u 
+ON i.user_id = u.id
+JOIN courses c 
+ON i.id = c.instructor_id
+GROUP BY u.id, u.name
+ORDER BY count_courses DESC;
 
 /* 3. Find all students who are enrolled in more than one course. Show their name and total enrolled courses. */
 SELECT u.name AS student_name, COUNT(e.course_id) AS total_enrolled_courses 
 FROM students s
-JOIN users AS u ON s.user_id = u.id
-JOIN enrollments e ON s.id = e.student_id
+JOIN users u 
+ON s.user_id = u.id
+JOIN enrollments e 
+ON s.id = e.student_id
 GROUP BY u.id, u.name
-HAVING COUNT(e.course_id) > 1;
+HAVING COUNT(e.course_id) > 1
+ORDER BY total_enrolled_courses DESC;
 
 /* 4.  List each course with the average rating it received in reviews. Include courses with no reviews (show NULL rating). */
-SELECT c.id, c.title AS course, ROUND(AVG(r.rating), 0) AS average_rating 
+SELECT c.title AS course, ROUND(AVG(r.rating), 0) AS average_rating 
 FROM courses c
-LEFT JOIN reviews r ON c.id = r.course_id
+LEFT JOIN reviews r 
+ON c.id = r.course_id
 GROUP BY c.id, c.title
+ORDER BY average_rating DESC;
 
 /* 5. List all students along with their total payment amount. Include students who haven’t made any payments (show 0). */
-SELECT u.id, u.name AS student, COALESCE(SUM(p.amount), 0) AS total_payment 
+SELECT u.name AS student, COALESCE(SUM(p.amount), 0) AS total_payment 
 FROM students s
-JOIN users u ON s.user_id = u.id
-LEFT JOIN payments p ON s.id = p.student_id
+JOIN users u 
+ON s.user_id = u.id
+LEFT JOIN payments p 
+ON s.id = p.student_id
 GROUP BY u.id
-ORDER BY u.id;
+ORDER BY u.id DESC;
 
 /* 6. Find courses that have more than 2 lessons. Show course title and number of lessons. */
 SELECT c.title AS course, count(l.id) AS total_lesson 
 FROM courses c
-JOIN lessons l ON c.id = l.course_id
+JOIN lessons l 
+ON c.id = l.course_id
 GROUP BY c.title
-HAVING count(l.course_id) > 30
+HAVING count(l.course_id) > 2
 ORDER BY total_lesson
 
 /* 7. Find the course(s) with the highest number of enrolled students. */
@@ -62,25 +75,31 @@ WHERE cc.student_count = (
 );
 
 /* 8. Show students who submitted at least one assignment, including student name and total submissions. */
-SELECT u.name, sa.total_assignment FROM 
+SELECT u.name, sa.total_assignment 
+FROM 
 (
     SELECT student_id, count(assignment_id) as total_assignment 
     FROM submissions 
     GROUP BY student_id
 ) as sa
-JOIN users u ON sa.student_id = u.id
+JOIN users u 
+ON sa.student_id = u.id
 ORDER BY total_assignment desc; 
 
 /* 9. List all instructors who have at least one course with no assignments. */
-SELECT DISTINCT u.name AS instructor_name
+SELECT DISTINCT u.name AS instructor_name, c.title AS course_name
 FROM courses c
-LEFT JOIN assignments a ON c.id = a.course_id
-JOIN users u ON c.instructor_id = u.id
+LEFT JOIN assignments a 
+ON c.id = a.course_id
+JOIN users u 
+ON c.instructor_id = u.id
 WHERE a.id IS NULL;
 
 /* 10. List students who have not submitted any assignments. */
 SELECT u.name AS student_name
 FROM students s
-JOIN users u ON s.user_id = u.id
-LEFT JOIN submissions sub ON s.id = sub.student_id
+JOIN users u 
+ON s.user_id = u.id
+LEFT JOIN submissions sub 
+ON s.id = sub.student_id
 WHERE sub.id IS NULL;
